41575b6bf5203fd98a7389a3fa30ea57c4858fac
Paulina <aniskiewicz.paulina@gmail.com>
2019-08-19 17:02:47 +0200

    SignOutButton added

---
+++ src/auth/SignOut.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import firebase from firebase;
+
+const SignOutButton = () => {
+    const signOut = () => {
+
+    };
+    return <Button color="secondary" onClick = {signOut}>Sign out</Button>
+}
+
+export default SignOutButton;
